486e2fb260d3e61274f978619b333d024e9e9661
tynan <tynanmcgeek@gmail.comm>
2023-07-06 22:45:07 -0700

    Fix treesitter settings not applying
    Turns out you can't just use "opts" in lazy, you have to do the full config because treesitter configs are in require("treesitter.configs") and not require("treesitter").

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,29 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            indent = {
-                enable = true,
-                disable = { "python" },
-            },
-            ensure_installed = {
-                "json",
-                "yaml",
-                "python",
-                "lua",
-                "rust",
-                "bash",
-                "javascript",
-                "markdown",
-                "regex",
-                "typescript",
-                "tsx",
-            },
-        },
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				indent = {
+					enable = true,
+					disable = { "python" },
+				},
+				ensure_installed = {
+					"json",
+					"yaml",
+					"python",
+					"lua",
+					"rust",
+					"bash",
+					"javascript",
+					"markdown",
+					"markdown_inline",
+					"regex",
+					"typescript",
+					"tsx",
+				},
+			})
+		end,
+	},
 
-    { "nvim-treesitter/nvim-treesitter-context", config = true },
+	{ "nvim-treesitter/nvim-treesitter-context", config = true },
 }
-
