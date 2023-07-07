941a7b46249b1902b00db22ddb7394aae50ba3fa
tynan <tynanmcgeek@gmail.com>
2023-07-07 11:54:24 -0700

    Add fidget to see LSP progress

--- init.lua
+++ init.lua
@@ -4,20 +4,20 @@ require("config.keymaps")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-    change_detection = {
-        enabled = false,
-        notify = false,
-    }
+	change_detection = {
+		enabled = false,
+		notify = false,
+	},
 })
