0e78c1d9c524023d624dec2170fd2cb5092772cc
tynan <tynanmcgeek@gmail.comm>
2023-07-08 00:32:49 -0700

    Add code actions panel to ignored file lists

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -1,53 +1,52 @@
-vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
-    callback = function()
-        vim.opt.formatoptions:remove({"c", "o"})
-    end,
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+	callback = function()
+		vim.opt.formatoptions:remove({ "c", "o" })
+	end,
 })
-
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 
 -- resize splits if window got resized
 vim.api.nvim_create_autocmd({ "VimResized" }, {
-  callback = function()
-    vim.cmd("tabdo wincmd =")
-  end,
+	callback = function()
+		vim.cmd("tabdo wincmd =")
+	end,
 })
-
 
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {
-    "PlenaryTestPopup",
-    "help",
-    "lspinfo",
-    "man",
-    "notify",
-    "qf",
-    "spectre_panel",
-    "startuptime",
-    "tsplayground",
-    "neotest-output",
-    "checkhealth",
-    "neotest-summary",
-    "neotest-output-panel",
-  },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-  end,
+	pattern = {
+		"PlenaryTestPopup",
+		"help",
+		"lspinfo",
+		"man",
+		"notify",
+		"qf",
+		"spectre_panel",
+		"startuptime",
+		"tsplayground",
+		"neotest-output",
+		"checkhealth",
+		"neotest-summary",
+		"neotest-output-panel",
+		"sagacodeaction",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
 })
 
 -- wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "gitcommit", "markdown" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
+	pattern = { "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
 })
