ba8fe5f5fa06de1be6a93418dfd53ace48f81e98
tynan <tynanmcgeek@gmail.com>
2023-10-16 23:14:25 -0700

    Add custom snippets

---
+++ luasnippets/go.lua
@@ -0,0 +1,13 @@
+return {
+	s(
+		"hand",
+		fmta(
+			[[
+            func <>(w http.ResponseWriter, r *http.Request) {
+                <>
+            }
+        ]],
+			{ i(1), i(0) }
+		)
+	),
+}
