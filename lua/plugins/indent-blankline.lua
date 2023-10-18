37bba29e46b860d36caed5b5155a5e08171d82c2
tynan <tynanmcgeek@gmail.com>
2023-10-17 23:40:08 -0700

    Remove ending scope guide from indent blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,32 +1,33 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	event = { "BufReadPost", "BufNewFile" },
-	main = "ibl",
-	opts = {
-		scope = {
-			highlight = {
-				-- i suppose these are not all necessarily different colors depending on the theme
-				"label",
-				"function",
-				"string",
-				"identifier",
-			},
-			show_start = false,
-		},
-		exclude = {
-			filetypes = {
-				"lspinfo",
-				"checkhealth",
-				"man",
-				"gitcommit",
-				"TelescopePrompt",
-				"TelescopeResults",
-				"help",
-				"mason",
-				"toggleterm",
-				"lazyterm",
-				"",
-			},
-		},
-	},
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    main = "ibl",
+    opts = {
+        scope = {
+            highlight = {
+                -- i suppose these are not all necessarily different colors depending on the theme
+                "label",
+                "function",
+                "string",
+                "identifier",
+            },
+            show_start = false,
+            show_end = false,
+        },
+        exclude = {
+            filetypes = {
+                "lspinfo",
+                "checkhealth",
+                "man",
+                "gitcommit",
+                "TelescopePrompt",
+                "TelescopeResults",
+                "help",
+                "mason",
+                "toggleterm",
+                "lazyterm",
+                "",
+            },
+        },
+    },
 }
