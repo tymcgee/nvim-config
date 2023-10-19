fdec78941b57e45dcc7495ae29b079b19f166592
tynan <tynanmcgeek@gmail.com>
2023-10-18 23:46:33 -0700

    Reformat session manager

--- lua/plugins/session-manager.lua
+++ lua/plugins/session-manager.lua
@@ -6,13 +6,13 @@ return {
     lazy = false,
     keys = {
         { "<leader>qs", "<cmd>SessionManager load_current_dir_session<cr>", desc = "Load session for CWD" },
-        { "<leader>ql", "<cmd>SessionManager load_last_session<cr>",        desc = "Load previous session" },
-        { "<leader>qc", "<cmd>SessionManager load_session<cr>",             desc = "Select session" },
+        { "<leader>ql", "<cmd>SessionManager load_last_session<cr>", desc = "Load previous session" },
+        { "<leader>qc", "<cmd>SessionManager load_session<cr>", desc = "Select session" },
     },
     opts = function()
         local config = require("session_manager.config")
         return {
-            autoload_mode = config.AutoloadMode.Disabled
+            autoload_mode = config.AutoloadMode.Disabled,
         }
-    end
+    end,
 }
