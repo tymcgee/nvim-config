6f8a5a279eef42aec26b71d560cf48a55df504a7
tynan <tynanmcgeek@gmail.com>
2023-10-20 23:57:26 -0700

    View hidden files with telescope find files
    (except for .git)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,13 @@ return {
         { "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
         { "<leader><leader>", "<cmd>Telescope live_grep<cr>", desc = "Grep (root dir)" },
         { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-        { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files (root dir)" },
+        {
+            "<leader><space>",
+            function()
+                require("telescope.builtin").find_files({ find_command = { "rg", "--files", "--hidden", "-g", "!.git" } })
+            end,
+            desc = "Find Files (root dir)",
+        },
         { "<leader>sb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
         { "<leader>sc", "<cmd>Telescope commands<cr>", desc = "Commands" },
         { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document diagnostics" },
