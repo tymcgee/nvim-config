179b587a2b28af6c370c755f2cf9770d8dcc9ea5
tynan <tynanmcgeek@gmail.com>
2023-10-21 23:58:10 -0700

    Fix html autotag config

--- lua/plugins/html-autotag.lua
+++ lua/plugins/html-autotag.lua
@@ -1,4 +1,3 @@
 return {
     "windwp/nvim-ts-autotag",
-    config = true,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,15 @@ return {
         },
         config = function()
             require("nvim-treesitter.configs").setup({
+                -- not sure why i have to specify all of these even though they're mentioned as defaults...
+                -- but it wasn't working in svelte files unless i did
+                autotag = {
+                    enable = true,
+                    enable_rename = true,
+                    enable_close = true,
+                    enable_close_on_slash = true,
+                    filetypes = { "html", "xml", "svelte", "markdown", "javascript", "typescript" },
+                },
                 highlight = {
                     enable = true,
                     -- custom_captures = {
