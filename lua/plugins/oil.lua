7aced9fabd3189a988ca06486cc5d5cf8924806b
tynan <tynanmcgeek@gmail.com>
2024-01-06 00:02:46 -0800

    Stop lazy loading oil
    fixes nvim . opening netrw

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,10 +1,12 @@
 return {
     "stevearc/oil.nvim",
+    lazy = false,
     keys = {
         { "<leader>e", "<cmd>Oil<cr>", desc = "Open file directory" },
         { "<leader>E", "<cmd>Oil .<cr>", desc = "Open CWD" },
     },
     opts = {
+        default_file_explorer = true,
         skip_confirm_for_simple_edits = true,
         view_options = {
             show_hidden = true,
