84d01e39a4768f9c4fe7cf5d67db45eb4994f40a
tynan <tynanmcgeek@gmail.com>
2024-01-06 00:02:26 -0800

    Remove junk

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -6,17 +6,14 @@ return {
             { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
             { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
         },
-        config = function(_, opts)
+        config = function()
             local leap = require("leap")
             leap.add_default_mappings(true)
-            -- "true" here means it'll overwrite any other keymaps
-            require("leap").add_default_mappings(true)
         end,
     },
     {
         "ggandor/flit.nvim",
         keys = function()
-            ---@type LazyKeys[]
             local ret = {}
             for _, key in ipairs({ "f", "F", "t", "T" }) do
                 ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
@@ -24,5 +21,5 @@ return {
             return ret
         end,
         opts = { labeled_modes = "nx" },
-    }
+    },
 }
