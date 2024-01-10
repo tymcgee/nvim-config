96e5a167572587de5991c3ed07042e4c12db0a82
tynan <tynanmcgeek@gmail.com>
2024-01-10 00:15:48 -0800

    Add neogit

---
+++ after/ftplugin/NeogitStatus.lua
@@ -0,0 +1,4 @@
+-- note that having this file open and opening neogit will
+-- replace the buffer with neogit lol
+vim.opt_local.number = true
+vim.opt_local.relativenumber = true
---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,22 @@
+return {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    keys = { { "<leader>gg", "<cmd>Neogit<cr>", desc = "Open git" } },
+    opts = {
+        -- this doesn't seem to work. maybe it does something else
+        disable_line_numbers = false,
+        mappings = {
+            status = {
+                -- so that i can use relative line jumps
+                ["1"] = false,
+                ["2"] = false,
+                ["3"] = false,
+                ["4"] = false,
+            },
+        },
+    },
+}
