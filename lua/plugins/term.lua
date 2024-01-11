2c556e940b5db3f3c1fd7b029078f961de942ead
tynan <tynanmcgeek@gmail.com>
2024-01-10 16:16:31 -0800

    Allow for non-fish shells in toggleterm
    Also made a new command to open a regular terminal at the bottom of the
    screen, and made the floating terminal have a curved border.

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -3,9 +3,19 @@ return {
     version = "*",
     config = function()
         vim.keymap.set("n", "<leader>ft", "<cmd>ToggleTerm direction=float<cr>")
+        vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<cr>")
+        local shell = "/bin/bash"
+        if vim.fn.executable("fish") == 1 then
+            shell = "/bin/fish"
+        elseif vim.fn.executable("zsh") == 1 then
+            shell = "/bin/zsh"
+        end
         require("toggleterm").setup({
             -- (because my vim.o.shell is set to bash for other reasons)
-            shell = "/bin/fish",
+            shell = shell,
+            float_opts = {
+                border = "curved",
+            },
         })
     end,
 }
