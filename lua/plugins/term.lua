59f504b7a8f1fc18863fb72574c5fed9200b5794
tynan <tynanmcgeek@gmail.com>
2024-01-16 15:13:29 -0800

    Use vim.fn.exepath to find path to shell executables

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -4,11 +4,11 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>ft", "<cmd>ToggleTerm direction=float<cr>")
         vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<cr>")
-        local shell = "/bin/bash"
+        local shell = vim.fn.exepath("bash")
         if vim.fn.executable("fish") == 1 then
-            shell = "/bin/fish"
+            shell = vim.fn.exepath("fish")
         elseif vim.fn.executable("zsh") == 1 then
-            shell = "/bin/zsh"
+            shell = vim.fn.exepath("zsh")
         end
         require("toggleterm").setup({
             -- (because my vim.o.shell is set to bash for other reasons)
