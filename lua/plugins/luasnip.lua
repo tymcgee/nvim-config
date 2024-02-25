f552bfa069cf35ccbc21dc552c95700c4c6c55ae
tynan <tynanmcgeek@gmail.com>
2024-02-24 16:37:57 -0800

    Remove snippet jump locations on normal mode

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -9,6 +9,21 @@ return {
             region_check_events = "InsertEnter",
             delete_check_events = "InsertLeave",
         })
+
+        -- when switching back to normal mode, remove all active snippet jump locations
+        -- from https://github.com/L3MON4D3/LuaSnip/issues/258#issuecomment-1429989436
+        vim.api.nvim_create_autocmd("ModeChanged", {
+            pattern = "*",
+            callback = function()
+                if
+                    ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+                    and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+                    and not require("luasnip").session.jump_active
+                then
+                    require("luasnip").unlink_current()
+                end
+            end,
+        })
     end,
     keys = {
         {
