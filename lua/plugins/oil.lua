220b38442e19078c461bfef1e5d8341f6190cd4c
tynan <tynanmcgeek@gmail.com>
2024-04-06 00:13:26 -0700

    Use 'q' to quit from oil explorer

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,16 +5,26 @@ return {
         { "<leader>e", "<cmd>Oil<cr>", desc = "Open file directory" },
         { "<leader>E", "<cmd>Oil .<cr>", desc = "Open CWD" },
     },
-    opts = {
-        default_file_explorer = true,
-        skip_confirm_for_simple_edits = true,
-        view_options = {
-            show_hidden = true,
-            is_always_hidden = function(name, bufnr)
-                return name == ".git"
+
+    config = function()
+        require("oil").setup({
+            default_file_explorer = true,
+            skip_confirm_for_simple_edits = true,
+            view_options = {
+                show_hidden = true,
+                is_always_hidden = function(name, bufnr)
+                    return name == ".git"
+                end,
+            },
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "oil" },
+            callback = function(event)
+                vim.keymap.set("n", "q", require("mini.bufremove").delete, { buffer = event.buf })
             end,
-        },
-    },
+        })
+    end,
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
 }
