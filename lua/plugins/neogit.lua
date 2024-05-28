ef496db9747288f85a3fa36e571da34e0dc260c6
tynan <tynanmcgeek@gmail.com>
2024-05-27 19:29:44 -0700

    Fix neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -6,16 +6,18 @@ return {
         "nvim-telescope/telescope.nvim",
     },
     keys = { { "<leader>gg", "<cmd>Neogit<cr>", desc = "Open git" } },
+    -- config = true,
     opts = {
         -- this doesn't seem to work. maybe it does something else
         disable_line_numbers = false,
         mappings = {
             status = {
                 -- so that i can use relative line jumps
-                ["1"] = false,
-                ["2"] = false,
-                ["3"] = false,
-                ["4"] = false,
+                -- (doesn't work anymore :( )
+                -- ["1"] = false,
+                -- ["2"] = false,
+                -- ["3"] = false,
+                -- ["4"] = false,
             },
         },
     },
