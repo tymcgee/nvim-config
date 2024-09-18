7e84c61e46de4c79fce298b72c68e7c78f28f175
tynan <tynanmcgeek@gmail.com>
2024-09-17 22:36:38 -0700

    update lualine config to include arrow

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,23 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = {
         "nvim-tree/nvim-web-devicons",
+        "otavioschwanck/arrow.nvim",
     },
-    config = true,
+    opts = function()
+        -- not sure why this has to be in a local function but whatever
+        local function arrow()
+            return require("arrow.statusline").text_for_statusline_with_icons()
+        end
+        return {
+            options = {
+                section_separators = "",
+                component_separators = "",
+            },
+            sections = {
+                lualine_c = { arrow, "filename" },
+                lualine_x = { "filetype" },
+            },
+        }
+    end,
+    -- config = true,
 }
