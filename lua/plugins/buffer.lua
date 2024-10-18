3aba04e51faa39a7b5e5c1f55fa9e25c56b32403
tynan <tynanmcgeek@gmail.com>
2024-10-17 22:10:19 -0700

    add keymaps to go to prev/next arrow file using H and L

--- lua/plugins/buffer.lua
+++ lua/plugins/buffer.lua
@@ -1,8 +1,13 @@
 return {
-  "otavioschwanck/arrow.nvim",
-  opts = {
-    show_icons = true,
-    leader_key = ';', -- Recommended to be a single key
-    buffer_leader_key = 'm', -- Per Buffer Mappings
-  }
+    "otavioschwanck/arrow.nvim",
+    config = function()
+        require("arrow").setup({
+            show_icons = true,
+            leader_key = ";", -- Recommended to be a single key
+            buffer_leader_key = "m", -- Per Buffer Mappings
+        })
+
+        vim.keymap.set("n", "H", require("arrow.persist").previous)
+        vim.keymap.set("n", "L", require("arrow.persist").next)
+    end,
 }
