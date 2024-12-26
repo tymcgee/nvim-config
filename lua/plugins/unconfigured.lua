2fd5813a47655f583b3044a50406c1536fd042e8
tynan <tynanmcgeek@gmail.com>
2024-12-26 14:34:21 -0800

    add guess-indent

--- lua/plugins/unconfigured.lua
+++ lua/plugins/unconfigured.lua
@@ -3,4 +3,5 @@ return {
     { "Vimjas/vim-python-pep8-indent" },
     { "windwp/nvim-autopairs", config = true },
     { "catgoose/templ-goto-definition", config = true, ft = { "go" } },
+    { "nmac427/guess-indent.nvim", config = true },
 }
