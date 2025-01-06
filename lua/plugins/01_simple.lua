1194c65622682113030bd7616c6dff25915078a6
tynan <tynanmcgeek@gmail.com>
2025-01-06 04:12:11 -0800

    small refactor of some files

--- lua/plugins/01_simple.lua
+++ lua/plugins/01_simple.lua
@@ -1,6 +1,8 @@
+local html_filetypes = { "html", "templ", "svelte", "tsx", "jsx", "vue" }
 return {
     { "Vimjas/vim-python-pep8-indent", ft = { "python" } },
     { "windwp/nvim-autopairs", config = true, event = "VeryLazy" },
     { "catgoose/templ-goto-definition", config = true, ft = { "go" } },
+    { "windwp/nvim-ts-autotag", ft = html_filetypes, opts = { opts = { enable_close_on_slash = true } } },
     { "nmac427/guess-indent.nvim", config = true, event = "BufReadPost" },
 }
--- lua/plugins/autotag.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "windwp/nvim-ts-autotag",
-    opts = {
-        opts = {
-            enable_close_on_slash = true,
-        },
-    },
-}
