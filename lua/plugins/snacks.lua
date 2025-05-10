ee5f0a1c1d2a908dd1fefbf97fc824d747398dfe
tynan <tynanmcgeek@gmail.com>
2025-05-10 10:16:54 -0700

    add option to open existing session on splash screen

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -20,6 +20,7 @@ return {
                     { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
                     { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
                     { icon = " ", key = "l", desc = "Restore Session", section = "session" },
+                    { icon = "󰉋 ", key = "o", desc = "Choose Session", action = ":SessionManager load_session" },
                     { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
                     { icon = " ", key = "q", desc = "Quit", action = ":qa" },
                     -- stylua: ignore end
