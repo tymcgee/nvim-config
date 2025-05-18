fd008700ea3621529169b1ab5ebbb2105c70a1f9
tynan <tynanmcgeek@gmail.com>
2025-05-17 20:38:05 -0700

    remove semantic highlighting from python

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,5 +1,4 @@
 vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
     callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
         if client == nil then
@@ -8,6 +7,12 @@ vim.api.nvim_create_autocmd("LspAttach", {
         if client.name == "ruff" then
             -- Disable hover in favor of Pyright
             client.server_capabilities.hoverProvider = false
+        end
+
+        -- my color scheme doesn't play very well with basedpyright's semantic tokens,
+        -- and the treesitter ones work fine, so just disable semantic token highlighting
+        if client.name == "basedpyright" then
+            client.server_capabilities.semanticTokensProvider = nil
         end
     end,
     desc = "LSP: Disable hover capability from Ruff",
