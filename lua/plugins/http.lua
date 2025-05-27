8c52b50ab49d9cd50d7e57b4f050be961850dfd7
tynan <tynanmcgeek@gmail.com>
2025-05-26 21:25:48 -0700

    add kuala.nvim for http requests

---
+++ lua/plugins/http.lua
@@ -0,0 +1,17 @@
+return {
+    "mistweaverco/kulala.nvim",
+    keys = {
+        { "<leader>Rs", desc = "Send request" },
+        { "<leader>Ra", desc = "Send all requests" },
+        { "<leader>Rb", desc = "Open scratchpad" },
+    },
+    ft = { "http", "rest" },
+    opts = {
+        global_keymaps = true,
+        global_keymaps_prefix = "<leader>r",
+        kulala_keymaps_prefix = "",
+        ui = {
+            formatter = true,
+        },
+    },
+}
