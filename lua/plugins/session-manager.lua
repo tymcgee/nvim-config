7553a795a30c17417f10ca7cfc43dd89644cd23b
tynan <tynanmcgeek@gmail.com>
2025-07-27 23:47:29 -0700

    don't autoload session in neovide

--- lua/plugins/session-manager.lua
+++ lua/plugins/session-manager.lua
@@ -7,8 +7,15 @@ return {
     keys = { { "<leader>qc", "<cmd>SessionManager load_session<cr>", desc = "Choose session" } },
     opts = function()
         local config = require("session_manager.config")
+        local autoload_mode = config.AutoloadMode.CurrentDir
+        if vim.g.neovide then
+            autoload_mode = config.AutoloadMode.Disabled
+            vim.schedule(function()
+                vim.cmd("SessionManager load_session")
+            end)
+        end
         return {
-            autoload_mode = config.AutoloadMode.CurrentDir,
+            autoload_mode = autoload_mode,
         }
     end,
 }
