faa68f1a3862659f7c9e05ef5747f40b425acbcc
tynan <tynanmcgeek@gmail.com>
2025-11-23 00:40:14 -0800

    use lsp in markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,5 +2,9 @@ return {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = "markdown",
     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
-    opts = {},
+    opts = {
+        completions = {
+            lsp = { enabled = true },
+        },
+    },
 }
