b0c6defdaeed5908803ca80715d577f352f0257b
tynan <tynanmcgeek@gmail.com>
2025-12-28 00:05:25 -0800

    kitty navigator instead of tmux navigator

--- lua/plugins/tmux-navigator.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "christoomey/vim-tmux-navigator",
-    keys = {
-        { mode = { "n", "t" }, "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
-        { mode = { "n", "t" }, "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
-        { mode = { "n", "t" }, "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
-        { mode = { "n", "t" }, "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
-    },
-}
