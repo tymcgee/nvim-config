57698f8f592c146c47da2fe03a6ead7153a4ab32
tynan <tynanmcgeek@gmail.com>
2026-02-09 03:11:09 -0800

    remove indent blankline for blink.indent
    works better and faster

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,33 +1,10 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    main = "ibl",
-    opts = {
-        scope = {
-            highlight = {
-                -- i suppose these are not all necessarily different colors depending on the theme
-                "label",
-                "function",
-                "string",
-                "identifier",
-            },
-            show_start = false,
-            show_end = false,
-        },
-        exclude = {
-            filetypes = {
-                "lspinfo",
-                "checkhealth",
-                "man",
-                "gitcommit",
-                "TelescopePrompt",
-                "TelescopeResults",
-                "help",
-                "mason",
-                "toggleterm",
-                "lazyterm",
-                "",
-            },
-        },
+    -- note: snacks also has an indent plugin, but i couldn't configure it very well. this one is very out-of-the-box, and fast
+    {
+        "saghen/blink.indent",
+        config = function()
+            -- stole this "background"-ish color from indent-blankline.nvim's default config... idk man
+            vim.api.nvim_set_hl(0, "BlinkIndent", { fg = "#31353f" })
+        end,
     },
 }
