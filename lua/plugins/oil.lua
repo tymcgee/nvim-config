590782af2e8bce0e57265ecd399b44d7425bc6ac
tynan <tynanmcgeek@gmail.com>
2026-02-11 22:50:11 -0800

    fix some issues with oil
    - remove the buffer faster so a quick ctrl+o doesn't go back to it
    - remove lsp rename autocommand since i'm pretty sure it's built in
    - delete buffer if file is deleted so exiting the oil buffer doesn't immediately give file not found

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -10,10 +10,11 @@ return {
         require("oil").setup({
             default_file_explorer = true,
             skip_confirm_for_simple_edits = true,
+            cleanup_delay_ms = 300, -- default is 2000, but i want a quick ctrl+o not to go back to oil
             view_options = {
                 show_hidden = true,
                 is_always_hidden = function(name, bufnr)
-                    return name == ".git"
+                    return name == ".git" or name == ".jj"
                 end,
             },
             keymaps = {
@@ -23,20 +24,35 @@ return {
             },
         })
 
-        -- press 'q' to close oil if it is open
+        -- autocommands
+        local augroup = vim.api.nvim_create_augroup("myoil", { clear = true })
+
+        -- press 'q' to close oil if it is open (doing it here instead of in autocommands.lua since this one is reliant on mini.bufremove to work properly)
         vim.api.nvim_create_autocmd("FileType", {
             pattern = { "oil" },
+            group = augroup,
             callback = function(event)
                 vim.keymap.set("n", "q", require("mini.bufremove").delete, { buffer = event.buf })
             end,
         })
 
-        -- LSP rename if renaming file
+        -- if you delete a file, also delete its buffer (taken from https://github.com/stevearc/oil.nvim/issues/612#issuecomment-2828601810)
         vim.api.nvim_create_autocmd("User", {
             pattern = "OilActionsPost",
-            callback = function(event)
-                if event.data.actions.type == "move" then
-                    Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+            group = augroup,
+            callback = function(e)
+                if e.data.actions == nil then
+                    return
+                end
+                for _, action in ipairs(e.data.actions) do
+                    if action.entry_type == "file" and action.type == "delete" then
+                        local file = action.url:sub(7)
+                        local bufnr = vim.fn.bufnr(file)
+
+                        if bufnr >= 0 then
+                            vim.api.nvim_buf_delete(bufnr, { force = true })
+                        end
+                    end
                 end
             end,
         })
