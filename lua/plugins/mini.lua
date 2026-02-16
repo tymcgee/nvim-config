062726eec39d1a9e47bf8af02dc0e6b4ff15fb47
tynan <tynanmcgeek@gmail.com>
2026-02-15 23:42:17 -0800

    remove mini.clue
    frankly i don't think i get much use out of the which-key-like functionality so i'm gonna try going without it for a bit and we'll see if i end up adding it back later

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -42,42 +42,6 @@ return {
                     update_n_lines = "",
                 },
             })
-
-            local clue = require("mini.clue")
-            clue.setup({
-                triggers = {
-                    -- Leader triggers
-                    { mode = { "n", "x" }, keys = "<Leader>" },
-                    -- `[` and `]` keys
-                    { mode = "n", keys = "[" },
-                    { mode = "n", keys = "]" },
-                    -- `g` key
-                    { mode = { "n", "x" }, keys = "g" },
-                    -- Marks
-                    { mode = { "n", "x" }, keys = "'" },
-                    { mode = { "n", "x" }, keys = "`" },
-                    -- Registers
-                    { mode = { "n", "x" }, keys = '"' },
-                    { mode = { "i", "c" }, keys = "<C-r" },
-                    -- `z` key
-                    { mode = { "n", "x" }, keys = "z" },
-                },
-                clues = {
-                    clue.gen_clues.square_brackets(),
-                    clue.gen_clues.builtin_completion(),
-                    clue.gen_clues.g(),
-                    clue.gen_clues.marks(),
-                    clue.gen_clues.registers(),
-                    clue.gen_clues.z(),
-                    { mode = "n", keys = "<Leader>b", desc = "+Buffers" },
-                    { mode = "n", keys = "<Leader>c", desc = "+Code" },
-                    { mode = "n", keys = "<Leader>cl", desc = "+LSP" },
-                    { mode = "n", keys = "<Leader>g", desc = "+Git" },
-                    { mode = "n", keys = "<Leader>s", desc = "+Search" },
-                    { mode = "n", keys = "<Leader>q", desc = "+Session" },
-                },
-                window = { delay = 300 },
-            })
         end,
     },
 }
