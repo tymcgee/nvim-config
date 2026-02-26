750a9a847a5cd43cd81f7e3d814304718a3ef8c6
tynan <tynanmcgeek@gmail.com>
2026-02-25 20:22:01 -0800

    move to main branch of treesitter
    now you need tree-sitter-cli for treesitter to work

--- lua/plugins/01_simple.lua
+++ lua/plugins/01_simple.lua
@@ -1,13 +1,12 @@
 return {
     { "navarasu/onedark.nvim", priority = 1000, config = function() require("onedark").load() end },
-    { "Vimjas/vim-python-pep8-indent", ft = { "python" } },
     { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
     { "windwp/nvim-ts-autotag", ft = require("html-filetypes"), opts = { opts = { enable_close_on_slash = true } } },
     { "nmac427/guess-indent.nvim", config = true, event = "BufReadPost" },
     { "dmmulroy/ts-error-translator.nvim", ft = { "typescript" }, config = true },
     { "lewis6991/gitsigns.nvim", event = { "BufReadPost" }, config = true },
     { "MagicDuck/grug-far.nvim", cmd = "GrugFar", config = true },
-    { "rmagatti/auto-session", lazy = false, opts = {} },
+    { "rmagatti/auto-session", lazy = false, config = true },
     -- stole this "background"-ish color from indent-blankline.nvim's default config... idk man
     { "saghen/blink.indent", config = function() vim.api.nvim_set_hl(0, "BlinkIndent", { fg = "#31353f" }) end },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,66 +1,77 @@
 local fileEntered = { "BufReadPost", "BufWritePost", "BufNewFile" }
 return {
-    {
-        { "nvim-treesitter/nvim-treesitter-context", config = true, event = fileEntered },
-        { "folke/ts-comments.nvim", opts = {}, event = fileEntered, enabled = vim.fn.has("nvim-0.10.0") == 1 },
-        { "andymass/vim-matchup", event = fileEntered },
+    { "nvim-treesitter/nvim-treesitter-context", config = true, event = fileEntered },
+    { "folke/ts-comments.nvim", opts = {}, event = fileEntered, enabled = vim.fn.has("nvim-0.10.0") == 1 },
+    { "andymass/vim-matchup", event = fileEntered },
 
-        {
-            "nvim-treesitter/nvim-treesitter",
-            checkout = "main",
-            build = ":TSUpdate",
-            event = fileEntered,
-            config = function()
-                require("nvim-treesitter.configs").setup({
-                    matchup = { enable = true },
-                    highlight = { enable = true },
-                    indent = { enable = true, disable = { "python" } },
-                    ensure_installed = {
-                        "json",
-                        "jsonc",
-                        "yaml",
-                        "toml",
-                        "csv",
-                        "xml",
-                        "python",
-                        "lua",
-                        "luadoc",
-                        "rust",
-                        "bash",
-                        "javascript",
-                        "markdown",
-                        "markdown_inline",
-                        "regex",
-                        "typescript",
-                        "tsx",
-                        "gitcommit",
-                        "gitignore",
-                        "git_config",
-                        "sql",
-                        "css",
-                        "scss",
-                        "html",
-                        "htmldjango",
-                        "go",
-                        "gomod",
-                        "gosum",
-                        "gotmpl",
-                        "http",
-                        "svelte",
-                        "templ",
-                        "vimdoc",
-                        "fish",
-                        "c",
-                        "cpp",
-                        "make",
-                        "ssh_config",
-                        "caddy",
-                        "java",
-                        "javadoc",
-                        "terraform",
-                    },
-                })
-            end,
-        },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        build = ":TSUpdate",
+        config = function()
+            local ts = require("nvim-treesitter")
+            local parsers = {
+                "json",
+                "yaml",
+                "toml",
+                "csv",
+                "xml",
+                "python",
+                "lua",
+                "luadoc",
+                "rust",
+                "bash",
+                "javascript",
+                "markdown",
+                "markdown_inline",
+                "regex",
+                "typescript",
+                "tsx",
+                "gitcommit",
+                "gitignore",
+                "git_config",
+                "sql",
+                "css",
+                "scss",
+                "html",
+                "htmldjango",
+                "go",
+                "gomod",
+                "gosum",
+                "gotmpl",
+                "http",
+                "svelte",
+                "templ",
+                "vimdoc",
+                "fish",
+                "c",
+                "cpp",
+                "make",
+                "ssh_config",
+                "caddy",
+                "java",
+                "javadoc",
+                "terraform",
+            }
+
+            -- this is a no-op for parsers that are already installed
+            ts.install(parsers)
+
+            local patterns = {}
+            for _, parser in ipairs(parsers) do
+                local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+                for _, pp in pairs(parser_patterns) do
+                    table.insert(patterns, pp)
+                end
+            end
+
+            vim.treesitter.language.register("groovy", "Jenkinsfile")
+
+            local start = function()
+                vim.treesitter.start()
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end
+            Config.new_autocmd("FileType", patterns, start, "Enable treesitter")
+        end,
     },
 }
